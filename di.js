9d70e6d2417fff3fe0a180d51fbe5ad1b3e41108
Marcel Valdez <marcelvaldez@fb.com>
2020-02-21 13:00:07 -0800

    Make di.js modern JS compatible

--- di.js
+++ di.js
@@ -1,58 +1,58 @@
-// Very simple dependency injection mehcanism
-(function () {
-    function mergeDeps(oldDeps, newDeps) {
-        if(typeof oldDeps !== 'object'
-        || oldDeps === null
-        || oldDeps === undefined) {
-            // scalar value or null value overridden
-            return newDeps;
-        }
+// Very simple dependency injection mechanism
 
-        if(typeof newDeps !== 'object') {
-            // value was overridden with scalar object
-            return newDeps;
-        }
+let deps = {};
 
-        if (newDeps === null || newDeps === undefined) {
-            // null values are ignored, can't override with null.
-            return oldDeps;
-        }
-
-        const oldKeys = Object.keys(oldDeps);
-        const newKeys = Object.keys(newDeps);
-        const oldEntries = oldKeys.filter(key => !newKeys.includes(key))
-            .map(key => {
-                const result = {};
-                result[key] = oldDeps[key];
-                return result;
-            });
-        const mergedEntries = oldKeys.filter(key => newKeys.includes(key))
-            .map(key => {
-                const result = {};
-                result[key] = mergeDeps(oldDeps[key], newDeps[key]);
-                return result;
-            });
-        const newEntries = newKeys.filter(key => !oldKeys.includes(key))
-            .map(key => {
-                const result = {};
-                result[key] = newDeps[key];
-                return result;
-            });
-        return Object.assign({}, ...oldEntries, ...mergedEntries, ...newEntries);
+function mergeDeps(oldDeps, newDeps) {
+    if(typeof oldDeps !== 'object'
+    || oldDeps === null
+    || oldDeps === undefined) {
+        // scalar value or null value overridden
+        return newDeps;
     }
 
-    function registerDeps(dependencies) {
-        window.di.deps = Object.freeze(mergeDeps(window.di.deps, dependencies));
+    if(typeof newDeps !== 'object') {
+        // value was overridden with scalar object
+        return newDeps;
     }
 
-    function inject(functor) {
-        const newDeps = functor(window.di.deps) || {};
-        registerDeps(newDeps);
+    if (newDeps === null || newDeps === undefined) {
+        // null values are ignored, can't override with null.
+        return oldDeps;
     }
 
-    window.di = {
-        registerDeps,
-        inject,
-        deps: {},
-    }
-})();
+    const oldKeys = Object.keys(oldDeps);
+    const newKeys = Object.keys(newDeps);
+    const oldEntries = oldKeys.filter(key => !newKeys.includes(key))
+        .map(key => {
+            const result = {};
+            result[key] = oldDeps[key];
+            return result;
+        });
+    const mergedEntries = oldKeys.filter(key => newKeys.includes(key))
+        .map(key => {
+            const result = {};
+            result[key] = mergeDeps(oldDeps[key], newDeps[key]);
+            return result;
+        });
+    const newEntries = newKeys.filter(key => !oldKeys.includes(key))
+        .map(key => {
+            const result = {};
+            result[key] = newDeps[key];
+            return result;
+        });
+    return Object.assign({}, ...oldEntries, ...mergedEntries, ...newEntries);
+}
+
+function registerDeps(dependencies) {
+    deps = Object.freeze(mergeDeps(deps, dependencies));
+}
+
+function inject(functor) {
+    const newDeps = functor(deps) || {};
+    registerDeps(newDeps);
+}
+
+export {
+    registerDeps,
+    inject,
+}
