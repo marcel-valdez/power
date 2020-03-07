9a03d3a09a7abf778b98ec7ee833dc5dbeff0d73
Marcel Valdez <marcel.valdez@live.com>
2020-03-07 14:24:29 -0800

    Set version of externally downloaded scripts

--- ui/renderer.mjs
+++ ui/renderer.mjs
@@ -1,5 +1,5 @@
-import { h, Component, render } from 'https://unpkg.com/preact?module';
-import htm from 'https://unpkg.com/htm?module';
+import { h, Component, render } from 'https://unpkg.com/preact@10.3.3?module';
+import htm from 'https://unpkg.com/htm@3.0.3?module';
 
 /**
  * Wrapper module to be the only place where libraries are fetched from the
