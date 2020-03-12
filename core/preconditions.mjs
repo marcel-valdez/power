13e82bf7ea9ebabf4ec40ff51d4722bd4dd65137
Marcel Valdez <marcel.valdez@live.com>
2020-03-12 12:52:42 -0700

    Add checkArgument precondition function

--- core/preconditions.mjs
+++ core/preconditions.mjs
@@ -20,10 +20,24 @@ const NULL_OR_UNDEFINED_MSG = 'Invalid argument value, null or undefined.';
 export function checkNotNullOrUndefined(
   obj, message = NULL_OR_UNDEFINED_MSG) {
   if (DISABLED) {
-    return;
+    return true;
   }
 
   if (utils.isNullOrUndefined(obj)) {
     throw new PreconditionFailedError(message);
   }
+
+  return true;
+}
+
+export function checkArgument(predicate = () => true, message = '') {
+  if (DISABLED) {
+    return true;
+  }
+
+  if (!predicate()) {
+    throw new PreconditionFailedError(message);
+  }
+
+  return true;
 }
