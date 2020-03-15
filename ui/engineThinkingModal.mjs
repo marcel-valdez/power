1096797332b5bae10145b1d16c56d35aff3b11e6
Marcel Valdez <marcel.valdez@live.com>
2020-03-15 01:13:31 -0700

    Make image reference a relative path

--- ui/engineThinkingModal.mjs
+++ ui/engineThinkingModal.mjs
@@ -4,7 +4,7 @@ export class EngineThinkingModal extends Component {
   render({ }, { }) {
     return html`<div class='modal thinking'>
 Mmmhhh... let me think<br/>
-<img src="/ui/thinking.gif" width="40" height="40"></img>
+<img src="ui/thinking.gif" width="40" height="40"></img>
 </div>`;
   }
 }
